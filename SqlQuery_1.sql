f9c5189370c9e695ead0b946a846e56a3353f77b
Caitlin Starkey <bluemoon.lunacy@gmail.com>
2023-10-28 23:44:45 -0400

    added SQL queries

---
+++ SqlQuery_1.sql
@@ -0,0 +1,3 @@
+﻿SELECT Customers.CustomerName, Orders.OrderID FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID WHERE Orders.OrderID = 10310;
+
+SELECT Suppliers.Address, Products.ProductID FROM Suppliers INNER JOIN Products ON Suppliers.SupplierID = Products.SupplierID WHERE Products.ProductID = 40;
